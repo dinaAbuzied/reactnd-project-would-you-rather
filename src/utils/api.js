f5e5caedc10ba35acb5285b6d8a3ad9b45fbe0bf
Dina Ahmed Hussien <dina.hussien@eg.ibm.com>
2019-08-16 21:05:52 +0200

    added api files

---
+++ src/utils/api.js
@@ -0,0 +1,22 @@
+import { 
+    _getQuestions, 
+    _getUsers, 
+    _saveQuestion, 
+    _saveQuestionAnswer 
+} from "./_DATA";
+
+export function getQuestions () {
+    return _getQuestions();
+}
+
+export function getUsers () {
+    return _getUsers();
+}
+
+export function saveQuestion (question) {
+    return _saveQuestion(question);
+}
+
+export function saveQuestionAnswer ({ authedUser, qid, answer }) {
+    return _saveQuestionAnswer ({ authedUser, qid, answer });
+}
