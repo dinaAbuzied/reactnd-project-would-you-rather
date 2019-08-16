fb58a39174da2873b18b3a682a0b8506b0cc72d1
Dina Ahmed Hussien <dina.hussien@eg.ibm.com>
2019-08-16 23:24:55 +0200

    added actions

---
+++ src/actions/users.js
@@ -0,0 +1,21 @@
+import { showLoading, hideLoading } from "react-redux-loading";
+import { getUsers } from "../utils/api";
+
+export const RECIEVE_USERS = "RECIEVE_USERS";
+
+export const recieveUsers = (users) => {
+    return {
+        type: RECIEVE_USERS,
+        users
+    }
+}
+
+export const handleRecieveUsers = () => {
+    return (dispatch) => {
+        dispatch(showLoading());
+        return getUsers().then(({users}) => {
+            dispatch(recieveUsers(users));
+            dispatch(hideLoading());
+        });
+    }
+}
