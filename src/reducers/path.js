05c832a66e1a68b3bc70a8bf0180115a9faec514
Dina Ahmed Hussien <dina.hussien@eg.ibm.com>
2019-08-17 22:14:37 +0200

    after login route to home if path = "/" or "/login"

--- src/reducers/path.js
+++ src/reducers/path.js
@@ -3,6 +3,9 @@ import { SET_PATH } from "../actions/path";
 const path = (state = '/', action) => {
     switch (action.type) {
         case SET_PATH:
+            if (action.path === "/login" || action.path === "/") {
+                return "/home";
+            }
             return action.path;
         default:
             return state;
