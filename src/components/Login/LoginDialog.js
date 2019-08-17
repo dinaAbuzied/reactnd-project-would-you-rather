59c347e987753ab4894c3d6af95100634562ca53
Dina Ahmed Hussien <dina.hussien@eg.ibm.com>
2019-08-17 15:43:31 +0200

    added login dialog box

--- src/components/Login/LoginDialog.js
+++ src/components/Login/LoginDialog.js
@@ -1,24 +1,75 @@
 import React, { Component } from 'react';
 import { connect } from "react-redux";
+import { setAuthedUser } from "../../actions/authedUser";
+import Card from 'react-bootstrap/Card';
+import Dropdown from 'react-bootstrap/Dropdown';
+import Image from 'react-bootstrap/Image';
+import Button from 'react-bootstrap/Button';
+import "./LoginDialog.css"
+
 
 class LoginDialog extends Component {
+    state = {
+        selectedUser: null
+    }
+    selectUser = (user) => {
+        this.setState({
+            selectedUser: user
+        })
+    }
     render() {
+        const { usersArr, signIn } = this.props;
         return (
-            <div className="login-dialog">
-                <header>
-                    <h1>Welcome to The Would You Rather App!</h1>
-                    <h2>Please sign in to continue</h2>
-                </header>
-                <main>
-                    <h3>Sgin in</h3>
-                </main>
-            </div>
+            <Card className="login-dialog">
+                <Card.Header>
+                    <Card.Title className="align-center">Welcome to The Would You Rather App!</Card.Title>
+                    <Card.Subtitle className="mb-2 text-muted align-center">Please sign in to continue</Card.Subtitle>
+                </Card.Header>
+                <Card.Body>
+                    <Dropdown>
+                        <Dropdown.Toggle variant="" id="users-dropdown">
+                            {this.state.selectedUser ?
+                                <div>
+                                    <div className="img-container">
+                                        <Image src={"../../assets/img/" + this.state.selectedUser.avatarURL} roundedCircle />
+                                    </div>
+                                    {this.state.selectedUser.name}
+                                </div> :
+                                "Select User"}
+                        </Dropdown.Toggle>
+
+                        <Dropdown.Menu>
+                            {
+                                usersArr.map((user) => (
+                                    <Dropdown.Item onSelect={() => this.selectUser(user)} key={user.id}>
+                                        <div className="img-container">
+                                            <Image src={"../../assets/img/" + user.avatarURL} roundedCircle />
+                                        </div>
+                                        {user.name}
+                                    </Dropdown.Item>
+                                ))
+                            }
+                        </Dropdown.Menu>
+                    </Dropdown>
+                    <Button onClick={() => signIn(this.state.selectedUser)} variant="primary" disabled={!this.state.selectedUser}>Sign in</Button>
+                </Card.Body>
+            </Card>
         )
     }
 }
 
 const mapStateToProps = ({ users }) => {
-    return { users }
+    return {
+        usersArr: Object.keys(users).map((user) => {
+            return users[user];
+        })
+    }
 }
 
-export default connect(mapStateToProps)(LoginDialog) 
+const mapDispatchToProps = dispatch => {
+    return {
+      signIn: (user) => dispatch(setAuthedUser(user))
+    }
+  }
+
+export default connect(mapStateToProps, mapDispatchToProps)(LoginDialog) 
