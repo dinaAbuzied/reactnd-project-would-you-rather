93caf84fe4bd903763f8892c2cf2efddd7bb74ae
Dina Ahmed Hussien <dina.hussien@eg.ibm.com>
2019-08-17 21:49:51 +0200

    added logout logic

--- src/actions/authedUser.js
+++ src/actions/authedUser.js
@@ -1,8 +1,15 @@
 export const SET_AUTHED_USER = "SET_AUTHED_USER";
+export const REMOVE_AUTHED_USER = "REMOVE_AUTHED_USER";
 
 export const setAuthedUser = (user) => {
     return {
         type: SET_AUTHED_USER,
         user
+    }
+}
+
+export const removeAuthedUser = () => {
+    return {
+        type: REMOVE_AUTHED_USER
     }
 }
