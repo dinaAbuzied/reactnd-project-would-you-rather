642d6feddfa49c1620d64fe6eac30145cee922b9
Dina Ahmed Hussien <dina.hussien@eg.ibm.com>
2019-08-17 19:37:20 +0200

    added location reducer and action

---
+++ src/actions/location.js
@@ -0,0 +1,8 @@
+export const SET_LOCATION = "SET_LOCATION";
+
+export const setLocation = (location) => {
+    return {
+        type: SET_LOCATION,
+        location
+    }
+}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from "redux";
 import authedUser from "./authedUser";
+import location from "./location";
 import questions from "./questions";
 import users from "./users";
 import { loadingBarReducer } from "react-redux-loading";
 
-export default combineReducers({authedUser, questions, users, loadingBar: loadingBarReducer});
+export default combineReducers({authedUser, location, questions, users, loadingBar: loadingBarReducer});
